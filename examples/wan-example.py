ae952bb355e7982e42127274244db4028d6d3f65
samuelt0 <stesfai@mit.edu>
2025-05-01 05:19:35 +0000

    Wan Model Kernels draft

--- examples/wan-example.py
+++ examples/wan-example.py
@@ -1,37 +1,25 @@
 import torch
-from diffusers import AutoencoderKLWan, WanPipeline
+from diffusers import WanTransformer3DModel, WanPipeline
 from diffusers.utils import export_to_video
 from nunchaku import NunchakuWanTransformer3DModel
 
 # Available models: Wan-AI/Wan2.1-T2V-14B-Diffusers, Wan-AI/Wan2.1-T2V-1.3B-Diffusers
 model_id = "Wan-AI/Wan2.1-T2V-14B-Diffusers"
-# vae = AutoencoderKLWan.from_pretrained(model_id, subfolder="vae", torch_dtype=torch.float32)
-# pipe = WanPipeline.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
-# pipe.to("cuda")
+transformer = NunchakuWanTransformer3DModel.from_pretrained(
+    "/home/stesfai/Wan2.1-T2V-14B-Diffusers/transformer", device="cuda", torch_dtype=torch.bfloat16
+)
+pipe = WanPipeline.from_pretrained(model_id, transformer=transformer, torch_dtype=torch.bfloat16)
+pipe.to("cuda")
 
 prompt = "A cat walks on the grass, realistic"
-#negative_prompt = "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards"
+negative_prompt = "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards"
 
-# output = pipe(
-#     prompt=prompt,
-#     negative_prompt=negative_prompt,
-#     height=480,
-#     width=832,
-#     num_frames=81,
-#     guidance_scale=5.0
-# ).frames[0]
-# export_to_video(output, "output.mp4", fps=15)
-
-
-transformer = NunchakuWanTransformer3DModel.from_pretrained(model_id)
-pipeline = WanPipeline.from_pretrained(
-    transformer=transformer, torch_dtype=torch.bfloat16
-).to("cuda")
-output = pipeline(
+output = pipe(
     prompt=prompt,
+    negative_prompt=negative_prompt,
     height=480,
     width=832,
-    num_frames=81,
+    num_frames=16, #81
     guidance_scale=5.0
 ).frames[0]
-export_to_video(output, "wanexample.mp4", fps=15)
+export_to_video(output, "wan-example.mp4", fps=15)
