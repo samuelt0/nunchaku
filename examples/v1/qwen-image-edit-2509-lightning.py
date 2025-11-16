b0544d938dd13515b2ff8a1efd77345a7972ab23
Muyang Li <lmxyy@users.noreply.github.com>
2025-11-16 12:16:52 -0500

    chore: update the qwen-image-edit-2509 example (#804)

--- examples/v1/qwen-image-edit-2509-lightning.py
+++ examples/v1/qwen-image-edit-2509-lightning.py
@@ -26,9 +26,9 @@ scheduler_config = {
 }
 scheduler = FlowMatchEulerDiscreteScheduler.from_config(scheduler_config)
 
-num_inference_steps = 4  # you can also use the 8-step model to improve the quality
+num_inference_steps = 8  # you can also use the 8-step model to improve the quality
 rank = 32  # you can also use the rank=128 model to improve the quality
-model_path = f"nunchaku-tech/nunchaku-qwen-image-edit-2509/svdq-{get_precision()}_r{rank}-qwen-image-edit-2509-lightningv2.0-{num_inference_steps}steps.safetensors"
+model_path = f"nunchaku-tech/nunchaku-qwen-image-edit-2509/lightning-251115/svdq-{get_precision()}_r{rank}-qwen-image-edit-2509-lightning-{num_inference_steps}steps-251115.safetensors"
 
 # Load the model
 transformer = NunchakuQwenImageTransformer2DModel.from_pretrained(model_path)
